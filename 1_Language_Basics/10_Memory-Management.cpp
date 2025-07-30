5180b63995bfe53f09020c43337c8af00844cbde
sadek <sadek7dev@gmail.com>
2025-07-30 23:22:26 +0100

    ADD memory management file

---
+++ 1_Language_Basics/10_Memory-Management.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    int myIntger;
+    float myFloat;
+    double myDouble;
+    char myChar;
+    
+    cout << "My intger size: " << sizeof(myIntger) << endl;
+    cout << "My float size: " << sizeof(myFloat) << endl;
+    cout << "My double size: " << sizeof(myDouble) << endl;
+    cout << "My char size: " << sizeof(myChar) << endl;
+}
