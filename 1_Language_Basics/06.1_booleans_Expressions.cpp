baf0bebdee9e375b7dc60623fbb14fc49a481286
sadek <sadek7dev@gmail.com>
2025-07-30 23:20:56 +0100

    Rname all the file (add 0)

---
+++ 1_Language_Basics/06.1_booleans_Expressions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+
+    int myAge = 26;
+    int workingAge = 19;
+
+    if (myAge >= 19) {
+
+        cout << "Old enough to work!";
+    }
+    else {
+        cout << "Not old enough to work!";
+    }
+}
