16b20dbb2406945671c2b94abbfe973c80da28c7
cpp-dev <sdx-rf17@users.noreply.github.com>
2025-10-15 16:18:35 +0100

    ADD: My ABS

---
+++ 4_Algorithms_Level_3/52_MyABS.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+float MyABS(float number){
+
+    return ( number > 0 ? number : ( number * -1 ) );
+}
+
+float ReadNumber(){
+
+    float number = 0;
+    cout << "Enter your number: ";
+    cin >> number;
+
+    return number;
+}
+
+int main () {
+
+    float number = ReadNumber();
+    cout << "My abs result: " << MyABS(number) << endl;
+    cout << "C++ abs result: " << abs(number) << endl;
+}
