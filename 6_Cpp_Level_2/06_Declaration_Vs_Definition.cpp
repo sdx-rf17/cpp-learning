2f9a384e67e719a928e6e34ae36bc3d8b8a2bcf4
cpp-dev <sdx-rf17@users.noreply.github.com>
2025-11-02 21:21:04 +0100

    ADD: Declaration_Vs_Definition

---
+++ 6_Cpp_Level_2/06_Declaration_Vs_Definition.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+//function declaration
+void addTwoNumbers(int num1, int num2);
+
+int main() {
+	addTwoNumbers(3 , 28);
+}
+
+//function definition
+void addTwoNumbers(int num1 , int num2 ){
+	
+	cout << "Result: " << num1 + num2 << endl;
+}
