0635f6e6893e12494a46d694eb2b52ca6a3d388e
cpp-dev <sdx-rf17@users.noreply.github.com>
2025-11-04 19:22:24 +0100

    ADD: Atomatic Variables

---
+++ 6_Cpp_Level_2/11_Automatic_Variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std; 
+
+
+int main(){
+    
+    auto A = 10; // Type Integer
+    auto B = 19.8; //Type Double
+    auto C = "something"; //Type String
+
+    cout << A << endl;
+    cout << B << endl;
+    cout << C << endl;
+
+    return 0;
+ }
